78a1b7641e0649ddf1e515750843d89e64ee7729
HARSH SHARMA <harsh.sharma.8986@gmail.com>
2025-04-28 17:49:37 +0530

    added intro

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,33 @@
-
 import { SpotlightNewDemo } from "./components/spotlight";
-
-
+import "./App.css"
 function App() {
   return (
-    <div className="w-full h-screen bg-black relative">
-   
+    <div className="font-FiraSans w-full min-h-screen bg-black overflow-hidden">
       <div className="absolute inset-0 z-0">
         <SpotlightNewDemo />
       </div>
-     
-      <div className="relative z-20 flex flex-col items-center justify-center h-full">
-        <div className="text-white text-8xl font-bold">Hi, I'm Harsh</div>
-        <p className="text-neutral-300 mt-4 text-center max-w-md">
-          Welcome to my portfolio! Explore my work and skills.
-        </p>
+
+      <div className="relative z-20 flex justify-center items-center md:p-20 h-full">
+      
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-8 items-center w-full max-w-7xl">
+          
+        <div className="text-white text-lg md:text-2xl text-center md:text-left">
+            <div className="text-white font-bold text-6xl sm:text-4xl xs:text-3xl md:text-8xl lg:text-10xl animate__animated animate__fadeIn">
+              Hi, I'm Harsh
+            </div>
+            <div className="mt-8 text-[27px]">third year CS undergrad here. I like to code, read, and play chess sometimes.</div>
+          </div>
+          <div className="flex justify-center md:justify-start">
+            <img
+              className="h-auto w-[80%] sm:w-[60%] md:w-[25rem] lg:w-[30rem] object-cover rounded-3xl shadow-lg"
+              src="https://res.cloudinary.com/dpwqggym0/image/upload/v1745837467/GpeocHdXAAAGCrl_bbbdaj.jpg"
+              loading="lazy"
+              alt="Harsh"
+            />
+          </div>
+
+          
+        </div>
       </div>
     </div>
   );
--- src/components/spotlight.tsx
+++ src/components/spotlight.tsx
@@ -3,7 +3,7 @@ import { Spotlight } from "./spotlight-new";
 
 export function SpotlightNewDemo() {
   return (
-    <div className="h-screen w-full rounded-md flex md:items-center md:justify-center bg-black/[0.96] antialiased bg-grid-white/[0.02] relative overflow-hidden">
+    <div className=" h-screen w-full rounded-md flex md:items-center md:justify-center bg-black/[0.96] antialiased bg-grid-white/[0.02] relative overflow-hidden">
       <Spotlight />
       <div className=" p-4 max-w-7xl  mx-auto relative z-10  w-full pt-20 md:pt-0">
       </div>
