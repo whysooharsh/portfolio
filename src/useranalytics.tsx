f106eee9750756c1f23ec3f6c349ccdf6e0dc5d0
HARSH SHARMA <harsh.sharma.8986@gmail.com>
2025-05-04 00:58:13 +0530

    fix : deployment

---
+++ src/useranalytics.tsx
@@ -0,0 +1,45 @@
+
+declare global {
+  interface Window {
+    dataLayer: any[];
+    gtag: (...args: any[]) => void;
+  }
+}
+
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
+import { GA_MEASUREMENT_ID } from "./config/analytics";
+
+export function useAnalytics() {
+  const location = useLocation();
+
+  useEffect(() => {
+    const loadScript = () => {
+      const script = document.createElement("script");
+      script.src = `https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`;
+      script.async = true;
+      document.head.appendChild(script);
+
+      const script2 = document.createElement("script");
+      script2.innerHTML = `
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', '${GA_MEASUREMENT_ID}', { page_path: window.location.pathname });
+      `;
+      document.head.appendChild(script2);
+    };
+
+    if (!window.gtag) {
+      loadScript();
+    }
+  }, []);
+
+  useEffect(() => {
+    if (typeof window.gtag === "function") {
+      window.gtag("config", GA_MEASUREMENT_ID, {
+        page_path: location.pathname,
+      });
+    }
+  }, [location]);
+}
